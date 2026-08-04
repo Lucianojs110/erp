91cb2072dc30d22a28f6b3d7f067298120da52e3
Luciano Oroño <lucianojs110@gmail.com>
2026-08-03 22:24:33 -0300

    arreglo cuit

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,45 +1,85 @@
-
 $(document).ready(function () {
 
     $('[data-toggle="tooltip"]').tooltip();
 
-    $("#tax_number").off('change keyup paste').on('change keyup paste', function () {
-        if ($(this).val().length == 11) {
+    $("#tax_number")
+        .off('change keyup paste')
+        .on('change keyup paste', function () {
+
+            const taxNumber = $(this).val().replace(/\D/g, '');
+
+            if (taxNumber.length !== 11) {
+                return;
+            }
+
             $.ajax({
-                headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                },
                 type: "POST",
                 url: '/contacts/consultarcuit',
                 dataType: "json",
                 data: {
-                    tax_number: $("#tax_number").val(),
+                    tax_number: taxNumber
                 },
-                success: function(data) {
+                success: function (data) {
                     console.log(data);
-                    if (data.persona == null) {
-                        toastr.warning('Cuit no Valido');
+
+                    const persona = data.persona;
+
+                    if (!persona) {
+                        toastr.warning('CUIT no válido');
+
                         $("#name").val('');
-                    } else {
-                        toastr.success('Cuit Valido');
-                        $("#name").val(data.persona.apellido + ' ' + data.persona.nombre);
-                        $("#city").val(data.persona.domicilio[0].localidad);
-                        $("#state").val(data.persona.domicilio[0].descripcionProvincia);
-                        $("#country").val('ARGENTINA');
-                        $("#landmark").val(data.persona.domicilio[0].calle + ' ' + data.persona.domicilio[0].numero);
+                        $("#supplier_business_name").val('');
+                        $("#city").val('');
+                        $("#state").val('');
+                        $("#country").val('');
+                        $("#landmark").val('');
+
+                        return;
                     }
+
+                    toastr.success('CUIT válido');
+
+                    const esPersonaJuridica =
+                        persona.tipoPersona === 'JURIDICA';
+
+                    const nombreCompleto = esPersonaJuridica
+                        ? persona.razonSocial
+                        : [persona.apellido, persona.nombre]
+                            .filter(Boolean)
+                            .join(' ');
+
+                    const domicilio = Array.isArray(persona.domicilio)
+                        ? persona.domicilio[0] || {}
+                        : persona.domicilio || {};
+
+                    const direccion = [
+                        domicilio.calle,
+                        domicilio.numero
+                    ].filter(Boolean).join(' ');
+
+                    $("#name").val(nombreCompleto || '');
+
+                    $("#supplier_business_name").val(
+                        esPersonaJuridica
+                            ? persona.razonSocial || ''
+                            : ''
+                    );
+
+                    $("#city").val(domicilio.localidad || '');
+                    $("#state").val(domicilio.descripcionProvincia || '');
+                    $("#country").val('ARGENTINA');
+                    $("#landmark").val(direccion);
                 },
-                error: function(){
-                    toastr.warning("Ocurrio un problema");
+                error: function (xhr) {
+                    console.error(xhr.responseText);
+                    toastr.warning('Ocurrió un problema al consultar el CUIT');
                 }
             });
+
             return false;
-        }
-    });
-
-
+        });
 
 });
-
-
-
-
-
